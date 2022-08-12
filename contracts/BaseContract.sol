95ca98afdb378bea170c88d239c8f8794b661999
tashun <tashun.chamara@gmail.com>
2022-08-12 23:38:41 +0530

    svg added

---
+++ contracts/BaseContract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract BaseContract {
+    string  constant  svgMetaData1 = "<svg version='1.1' xmlns='http://www.w3.org/2000/svg'xmlns:xlink='http://www.w3.org/1999/xlink'viewBox='0 0 3 2'preserveAspectRatio='xMidYMid'><g fill='none' stroke='black' stroke-width='0.02'><circle cx='1' cy='1' r='0.5' fill=";
+    string  constant  svgMetaData2 = "/><ellipse cx='0.75' cy='0.85' rx='0.05' ry='0.04' fill="
+    string  constant  svgMetaData3 = "/><ellipse cx='1.25' cy='0.85' rx='0.05' ry='0.04' fill="
+    string  constant  svgMetaData4 = "/><polygon points='1,0.9 1.1,1.1 0.9,1.1' fill="
+    string  constant  svgMetaData5 = "/><path d='M0.75,1.25 L1.25,1.25 A0.5,0.5 0 0 1 0.75,1.25' fill="
+    string  constant  svgMetaData6 = "/></g></svg>"
+}
